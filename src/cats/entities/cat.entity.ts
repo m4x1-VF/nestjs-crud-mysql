0a2b83f5dca746e728d87f065af1b5ef05692870
Maximiliano Vilariño <maxi.vilarino@gmail.com>
2023-11-07 00:53:51 +0100

    Define cat entity

---
+++ src/cats/entities/cat.entity.ts
@@ -0,0 +1,19 @@
+import { Column, DeleteDateColumn, Entity } from 'typeorm';
+
+@Entity()
+export class Cat {
+  @Column({ primary: true, generated: true })
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  age: number;
+
+  @Column()
+  breed: string;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
